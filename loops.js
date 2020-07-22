b73c38e2426e338af05b1d70587698e368c80dc6
Will Kotheimer <wkotheimer@gmail.com>
2020-07-21 21:18:57 -0500

    the-dom

--- loops.js
+++ loops.js
@@ -1,17 +1,17 @@
-"use strict";
+// "use strict";
 
-const colors = ['red', 'orange', 'blue', 'yellow', 'green']
+// const colors = ['red', 'orange', 'blue', 'yellow', 'green']
 
-const colorLoop = () => {
-    let domString = '';
+// const colorLoop = () => {
+//     let domString = '';
 
-    for (let i = 0; i < colors.length; i++) {
-        domString += `<h1>${colors[i]}</h1>`;
-    }
-    console.log(domString);
-    return domString;
-}
+//     for (let i = 0; i < colors.length; i++) {
+//         domString += `<h1>${colors[i]}</h1>`;
+//     }
+//     console.log(domString);
+//     return domString;
+// }
 
-document.getElementById('container').innerHTML = colorLoop();
+// document.getElementById('container').innerHTML = colorLoop();
 
-//Document.getElementByID('container').innerhtml = colorLoop();
+// //Document.getElementByID('container').innerhtml = colorLoop();
